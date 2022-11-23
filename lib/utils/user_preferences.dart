9da8907a70a1a76a27cd941d9b57df5754fd3ab8
GGGGGGalih <kusumagalih062002@gmail.com>
2022-11-23 16:12:44 +0700

    UTS

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,12 @@
+import 'package:user_profile_ii_example/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath: 'https://www.pngdownload.id/png-u1z8o0/',
+    name: 'STTB Galih Cahya Kusuma 20552011183',
+    email: 'kusumagalih062002@gmail.com',
+    about:
+        'Aplikasi Profile Sosial Media yang mengadopsi sistem seperti sosial media pada umumnya. Bisa digabung dengan projek fullstack lain',
+    isDarkMode: false,
+  );
+}
